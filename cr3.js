40643f6fcacf0f45561e9cd040dc60a325d79086
abdullah99aldarwish <abdallah.ardarwesh@gmail.com>
2022-07-02 15:27:29 +0200

    push

---
+++ cr3.js
@@ -0,0 +1,13 @@
+function calculateInvoice(starterPrice, DrinkPrice, DessertPrice, SalatPrice) {
+    let sum = (starterPrice + DrinkPrice + DessertPrice + SalatPrice);
+    console.log(sum);
+}
+calculateInvoice(10, 5, 8, 4)
+
+
+
+function calculateINvoice(starterPrice, DrinkPrice, DessertPrice, SalatPrice) {
+    let result = (starterPrice + DrinkPrice + DessertPrice + SalatPrice) / 10;
+    console.log(result);
+}
+calculateINvoice(10, 5, 8, 4)
